c23296a88b1cae9538e3b071739b869a1956717e
rop2024 <rohitpandey231456@gmail.com>
2025-01-31 14:36:19 +0530

    add: basic_cal

---
+++ 01_basics/01_basic_cal.js
@@ -0,0 +1,14 @@
+let a = 2;
+let b = 4;
+
+let add = a+b;
+let sub = a-b;
+let pro = a*b;
+let div = a/b;
+let rem = a%b;
+
+console.log(add);
+console.log(sub);
+console.log(pro);
+console.log(div);
+console.log(rem);
